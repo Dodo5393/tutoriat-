88485be14601690ebb4c4a893dd8e387049da05b
Dodo5393 <dominikkowalczyk5393@gmail.com>
2023-03-02 08:51:02 +0100

    data base

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template ,request
+from flask import Flask, render_template ,request,g
+import sqlite3
+
+app_info = {
+    'app_file' : r'C:\Users\domin\OneDrive\Documents\project\udemi\flask2\tutoriat-\data\engrams.db'
+}
 
 app =  Flask(__name__)
 
+
+def get_db():
+    if not hasattr(g,'sqlite_db'):
+        conn = sqlite3.connect(app_info['app_file'])
+        conn.row_factory = sqlite3.Row
+        g.sqlite_db =  conn
+    return  g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite3_db'):
+        g.sqlite_db.close()
 
 class Gender:
     def __init__(self, code, name, symbol):
@@ -49,6 +66,7 @@ def formula():
             imie = request.form['imie'] 
             if imie == '':
                 imie = 'none'
+            
         if 'nazwisko' in request.form:
             nazwisko = request.form['nazwisko']
             if nazwisko == '':
@@ -60,8 +78,17 @@ def formula():
         if 'gender' in request.form:
             gender = request.form['gender']
 
+
+        db = get_db()
+        sql_comand = 'insert into ofengrams(name, sname, old) values(?,?,?)'
+        db.execute(sql_comand, [imie , nazwisko, wiek])
+        db.commit()
         return render_template('formula_r.html',imie=imie , nazwisko=nazwisko, wiek=wiek,gender=gender,
                                gender_info=type_g.get_by_code(gender))  
     
+@app.route('/history')
+def history():
+    db=get_db()
+    sqlite3
 if __name__ == '__main__':
     app.run()
